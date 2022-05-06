d2c70c5c888f5e7b1147fa0fd8d0e21c1a684bcb
Laurentiu <dumitrulaurentiu64@gmail.com>
2022-05-06 16:21:17 +0300

    before email

--- src/TOADD.js
+++ src/TOADD.js
@@ -1,22 +1,35 @@
-// SHOP
 
 
-// FINISH PROFILE SECTION
+// DO THE FLYERS                        NOT DONE
 
+// SEND EMAIL                        ALMOST DONE
 
-// ADD ALL FIELDS FOR EMPLOYEES
+// MAKE THE APP FANCY                   NOT DONE
 
+// ADD ALL FIELDS FOR EMPLOYEES             DONE
+/////////////////
+// Name
+// Lastname ( FAMILY NAME )
+// Position
+// Department
+// DateOfJoining
+// PhotoFileName
+// Base_Salary
+// Increase
+// Gross_Prizes
+// Deductions
+////////////////
+// Gross_Total
+// Taxable_Gross
+// Tax
+// CAS
+// CASS
+// Net_Salary
 
-// DO THE CONFIGURATION PAGE
+// FINISH PROFILE SECTION                   DONE
 
+// CHANGE PASSWORD                          DONE
 
-// DO THE FLYERS
-
-
-// MAKE THE APP FANCY
-
-
-// SEND EMAIL       DONE
-// CHANGE PASSWORD  DONE
+// DO THE CONFIGURATION PAGE                DONE
 
 // BCJRE-GADZG-5V9P3
